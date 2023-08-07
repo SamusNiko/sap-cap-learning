52245feda19f1c00c71aa36f5a4f37dafb45fd49
Mikalai Samusevich <samusevich98@gmail.com>
2023-08-07 13:38:42 +0000

    add hybrid mode

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -58,19 +58,19 @@ module.exports = cds.service.impl(async function () {
           As this is not possible, the risk entity and the business partner entity are in different systems (SAP BTP and S/4 HANA Cloud), 
           if there is such an expand, remove it
         */
-        console.log("req: ", req.query);
+        // console.log("req: ", req.query);
         if (!req.query.SELECT.columns) return next();
 
         const expandIndex = req.query.SELECT.columns.findIndex(
             ({ expand, ref }) => expand && ref[0] === "bp"
         );
-        console.log(req.query.SELECT.columns, " expand Index: ", expandIndex);
+        // console.log(req.query.SELECT.columns, " expand Index: ", expandIndex);
         if (expandIndex < 0) return next();
 
         req.query.SELECT.columns.splice(expandIndex, 1);
         if (req?.query?.SELECT?.columns &&
             !req.query.SELECT.columns.find((column) => {
-                console.log("columnFFFF:   ", column);
+                // console.log("columnFFFF:   ", column);
                 if (typeof column !== "string") {
                     return column.ref.find((ref) => ref == "bp_BusinessPartner")
                 }
@@ -103,7 +103,7 @@ module.exports = cds.service.impl(async function () {
                 })
             );
         } catch (error) {
-            console.log("error: ", error);
+            // console.log("error: ", error);
         }
     });
 });
